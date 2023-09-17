f3e3aee0d366118669b4a5102b297154f28724d9
jodyboy <elite.crew66@gmail.com>
2023-09-17 11:05:10 +0700

    MultiStepForm using React

--- src/Container/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AccountInformation } from './AccountInformation';
-export { default as AddressInformation } from './AddressInformation';
-export { default as PersonalInformation } from './PersonalInformation';
